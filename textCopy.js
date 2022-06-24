d2574609b4a71a8bb2eb3c0b39e377c85183e8d2
akplee <ololee123@gmail.com>
2022-06-24 15:45:30 +0600

    Copiyng to clipboard
    Copiyng to clipboard by clicking on hex color name

--- textCopy.js
+++ textCopy.js
@@ -1,5 +1,12 @@
 function copyToClipboard(itemNumber) {
+  const paragraph = document.getElementsByTagName("hex")[itemNumber];
+  
+  if(paragraph.innerText != "Color") {
     const cb = navigator.clipboard;
-    const paragraph = document.getElementsByTagName("hex")[itemNumber];
     cb.writeText(paragraph.innerText);
-  }
+
+    var hexText = paragraph.innerHTML;
+    paragraph.innerHTML = "Copied!";
+    setTimeout(() => { paragraph.innerHTML = hexText }, 1000);
+  }  
+}
