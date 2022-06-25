37c18c78bc17b672774df634bf7d3e6f9bcffb21
akplee <ololee123@gmail.com>
2022-06-25 15:03:54 +0600

    Color lock

--- colorPalette.js
+++ colorPalette.js
@@ -9,16 +9,38 @@ function getHex(){
     return color;
 }
 
+function lockBox(itemNumber){
+     var box = document.getElementsByTagName("box")[itemNumber];
+     var lock = document.getElementsByTagName("lock")[itemNumber];
+
+     if(lock.innerHTML == "Lock"){
+        box.classList.toggle('locked');
+        lock.innerHTML = "Locked";
+     }
+     else{
+        box.classList.toggle('locked');
+        lock.innerHTML = "Lock";
+     }
+}
+
 function changeColor(itemNumber){
     var hex = getHex();
-    document.getElementsByTagName("hex")[itemNumber].innerHTML = hex;
-    document.getElementsByTagName("BOX")[itemNumber].style.backgroundColor = hex;
+
+    if (document.getElementsByTagName("lock")[itemNumber].innerHTML == "Lock"){
+        document.getElementsByTagName("hex")[itemNumber].innerHTML = hex;
+        document.getElementsByTagName("box")[itemNumber].style.backgroundColor = hex;
+    }
+    
 }
 
 function changeAllColors(){
     for (var i = 0; i < 5; i++) {
         var hex = getHex();
-        document.getElementsByTagName("hex")[i].innerHTML = hex;
-        document.getElementsByTagName("BOX")[i].style.backgroundColor = hex;
+        if (document.getElementsByTagName("lock")[i].innerHTML == "Lock"){
+            document.getElementsByTagName("hex")[i].innerHTML = hex;
+            document.getElementsByTagName("box")[i].style.backgroundColor = hex;
+        }
     }
 }
+
+
